6225169962a8db4a32e50bb4ae7e53c816e68ef3
Yogesh-1994 <69709013+Yogesh-1994@users.noreply.github.com>
2020-09-26 11:55:28 +0530

    initial commit

--- Groceries association rule.R
+++ Groceries association rule.R
@@ -1,22 +1,54 @@
+groceries <- read.transactions(file.choose(),sep = ",")
+View(groceries)
+itemFrequencyPlot(groceries, topN=10)
+inspect(groceries[1:10])
+class(groceries)
+summary(groceries)
 install.packages("arules")
-data()
 library(arules)
-data("Groceries")
-?Groceries
-inspect(Groceries[1:10])
-class(Groceries)
-summary(Groceries)
-itemFrequencyPlot(Groceries,topN =10,col ="blue")
-install.packages("aruleviz")
+install.packages("arulesViz")
 library(arulesViz)
-arules<-apriori(Groceries,parameter = list(support =0.002,confidence =0.6,minlen =2))
+data <- as.matrix(groceries)
+data2 <- as(gro, "transactions")
+arules <- apriori(gro, parameter = list(support=0.002, confidence=0.5, minlen= 2))
 arules
-inspect(arules[1:10])
-inspect(head(sort(arules,by="lift")))
+inspect(arules[1:20,])
+rules <- sort(arules, by="lift")
+inspect(arules[1:20,])
+inspect <- head(sort(arules, by="lift"))
 head(quality(arules))
 plot(arules)
-windows()
-plot(arules,method = "grouped")
-plot(arules[1:7],method = "graph")
-write(arules,file = "a_rules.csv",sep=",")
-getwd()
+plot(arules, method="grouped")
+plot(arules, method = "graph")
+
+
+#iteration 1 
+
+
+arules <- apriori(gro, parameter = list(support=0.0021, confidence=0.6, minlen= 3))
+arules
+inspect(arules[1:20,])
+arules <- sort(arules, by="lift")
+inspect(arules[1:20,])
+inspect <- head(sort(arules, by="lift"))
+head(quality(arules))
+plot(arules)
+plot(arules, method="grouped")
+plot(arules, method = "graph")
+
+#iteration 2
+
+arules <- apriori(gro, parameter = list(support=0.0022, confidence=0.4, minlen= 4))
+arules
+inspect(arules[1:20,])
+rules <- sort(arules, by="lift")
+inspect(arules[1:20,])
+inspect <- head(sort(arules, by="lift"))
+head(quality(arules))
+plot(arules)
+plot(arules, method="grouped")
+plot(arules, method = "graph")
+
+
+
+
